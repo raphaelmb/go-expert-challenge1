6d5ce129d420a60f910ef7480f318cb5e8cc6968
Raphael Berger <raphaelmb@gmail.com>
2022-10-27 14:19:40 -0300

    Save to db

--- server/main.go
+++ server/main.go
@@ -18,6 +18,8 @@ const (
 	PORT    = ":8080"
 )
 
+var DB *sql.DB
+
 type Cotacao struct {
 	USDBRL struct {
 		Code       string `json:"code"`
@@ -35,21 +37,30 @@ type Cotacao struct {
 }
 
 func main() {
-	prepareDb()
 	db, _ := sql.Open("sqlite3", "db.sqlite")
 	defer db.Close()
-
+	DB = db
+	prepareAndConnectDB()
 	http.HandleFunc("/cotacao", CotacaoHandler)
 	http.ListenAndServe(PORT, nil)
 }
 
-func prepareDb() {
+func prepareAndConnectDB() {
 	os.Remove("db.sqlite")
 	file, err := os.Create("db.sqlite")
 	if err != nil {
 		panic(err)
 	}
 	file.Close()
+
+	_, err = DB.Exec(`
+	CREATE TABLE IF NOT EXISTS cotacao (
+		code TEXT, code_in TEXT, name TEXT, high TEXT, low TEXT, var_bid TEXT, pct_change TEXT, bid TEXT, ask TEXT, timestamp TEXT, create_date TEXT
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func CotacaoHandler(w http.ResponseWriter, r *http.Request) {
@@ -97,10 +108,26 @@ func GetCotacao(url string) (*Cotacao, error) {
 		return nil, err
 	}
 
+	err = SaveCotacao(DB, &c)
+	if err != nil {
+		return nil, err
+	}
+
 	return &c, nil
 }
 
-// TODO: save to database
-// func SaveCotacao(db *sql.DB, cotacao *Cotacao) error {
+// TODO: add context timeout
+func SaveCotacao(db *sql.DB, cotacao *Cotacao) error {
+	stmt, err := db.Prepare("insert into cotacao(code, code_in, name, high, low, var_bid, pct_change, bid, ask, timestamp, create_date) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
-// }
+	_, err = stmt.Exec(cotacao.USDBRL.Code, cotacao.USDBRL.Codein, cotacao.USDBRL.Name, cotacao.USDBRL.High, cotacao.USDBRL.Low, cotacao.USDBRL.VarBid, cotacao.USDBRL.PctChange, cotacao.USDBRL.Bid, cotacao.USDBRL.Ask, cotacao.USDBRL.Timestamp, cotacao.USDBRL.CreateDate)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
